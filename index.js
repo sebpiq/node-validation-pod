1121edae7f6a6894c77ce485544a4eaa825fec83
Sébastien Piquemal <sebpiq@gmail.com>
2014-11-27 11:39:27 +0200

    refactored as object

--- index.js
+++ index.js
@@ -1,84 +1,119 @@
 var _ = require('underscore')
-  , chai = require('chai')
   , async = require('async')
 
-// Validates the object `obj`, by running all `validators` `{attrName: func(val[, done])}` on it.
-// Validation errors are then stored in `validationErrors` object.
-// The path of unvalid attributes is constructed by appending the name of the unvalid 
-// attribute to `prefix`.
-// `beforeAfter` contain hooks `before()`, `after()` and `done(err, obj, validationErrors)`
-var validateObject = exports.validateObject = function(prefix, obj, validationErrors, beforeAfter, validators) {
-  var asyncValid = []
-    , isValid = true
 
-  var _handleError = function(err) {
-    if (err instanceof chai.AssertionError) {
-      validationErrors[prefix] = err.message
-      isValid = false
-    } else throw err
-  }
-
-  var _doFinally = function() {
-    var unknownAttrs = _.difference(_.keys(obj), _.keys(validators))
-    if (unknownAttrs.length)
-      _handleError(new chai.AssertionError('unknown attributes [' + unknownAttrs.join(', ') + ']'))
-    if (isValid && beforeAfter.after) {
-      try { beforeAfter.after.call(obj) } catch (err) { _handleError(err) }
-    }
-    if (beforeAfter.done) beforeAfter.done(null, obj, validationErrors)
-  }
-
-  // Run the `before` hook
-  if (beforeAfter.before) {
-    try { beforeAfter.before.call(obj) } catch (err) {
-      _handleError(err)
-      _doFinally()
-      return
-    }
-  }
-
-  // Run validators for all attributes
-  async.series(_.pairs(validators).map(function(p) {
-    var attrName = p[0]
-      , func = p[1]
-      , val = obj[attrName]
-    return validate.bind(obj, prefix + '.' + attrName, validationErrors, val, func)
-  }), function(err, results) {
-    if (err) return _handleError(err)
-    if (_.some(results, function(r) { return r !== null })) isValid = false
-    _doFinally()
-  })
+var Validator = exports.Validator = function(validators, beforeAfter) {
+  this.validators = validators
+  beforeAfter = beforeAfter || {}
+  this.before = beforeAfter.before
+  this.after = beforeAfter.after
 }
 
-// Validates `val` and if validation error, store it to `validationErrors[prefix]`
-// The validator is `func(val[, done])`.
-// Once the validation is finished, `done(err, validationErr)` is called.
-var validate = exports.validate = function(prefix, validationErrors, val, func, done) {
 
-  var _handleError = function(err) {
-    if (err instanceof chai.AssertionError) {
-      validationErrors[prefix] = err.message
-      if (done) done(null, err)
-    } else if (done) done(err, null)
-    else throw err
-  }
+_.extend(Validator.prototype, {
 
-  // Both async and sync validation, in case calling the function directly throws an error.
-  // For asynchronous validation, errors are returned as the first argument of the callback.
-  if (func.length === 2) {
-    try {
-      func.call(this, val, function(err) {
-        if (err) _handleError(err)
-        else if (done) done(null, null)
+  handleError: function(err) {},
+
+  run: function(obj, opts, done) {
+
+    var _doFinally = function() {
+      // Check for unknown attributes
+      var unknownAttrs = _.difference(_.keys(obj), _.keys(self.validators))
+      if (unknownAttrs.length)
+        validationErrors[prefix] = 'unknown attributes [' + unknownAttrs.join(', ') + ']'
+
+      // Run the `after` hook only if there is no validation error.
+      if (isValid && self.after) {
+        try {
+          self.after.call(obj)
+        } catch (err) {
+          if(!self._handleError(err)) return done(err)
+        }
+      }
+      done(null, obj, validationErrors)
+    }
+
+    var _handleError = function(err) {
+      var returned = self.handleError(err)
+      if (!returned) return false
+      else {
+        validationErrors[prefix] = returned
+        isValid = false
+        return true
+      }
+    }
+
+    // Handling variable number of arguments and options
+    var defaults = {validationErrors: {}, prefix: ''}
+    if (arguments.length === 3)
+      _.defaults(opts, defaults)
+    else if (arguments.length === 2) {
+      done = opts
+      opts = defaults
+    } else throw new Error('unvalid arguments')
+
+    var self = this
+      , attrNames = _.keys(this.validators)
+      , validationErrors = opts.validationErrors
+      , prefix = opts.prefix
+      , isValid = true
+
+    // Run the `before` hook
+    if (this.before) {
+      try {
+        this.before.call(obj)
+      } catch (err) {
+        if (this._handleError(err)) _doFinally()
+        else done(err)
+        return
+      }
+    }
+
+    // Run validators for all attributes, and collect validation errors
+    async.series(_.map(attrNames, function(attrName) {
+      return _.bind(self.validate, self, obj, attrName)
+    }), function(err, results) {
+      if (err) return done(err)
+      _.forEach(_.zip(attrNames, results), function(p) {
+        if (p[1]) {
+          validationErrors[prefix + '.' + p[0]] = p[1]
+          isValid = false
+        }
       })
-    } catch (err) { _handleError(err) }
+      _doFinally()
+    })
 
-  // Synchronous validation only
-  } else {
-    try {
-      func.call(this, val)
-      if (done) done(null, null)
+  },
+
+  // Validates `attrName` of `obj` and calls `done(err, validationErrMsg)` is called.
+  validate: function(obj, attrName, done) {
+    var self = this
+      , val = obj[attrName]
+      , validator = this.validators[attrName]
+
+    var _asyncCb = function(err) {
+      if (err) _handleError(err)
+      else done()
     }
-    catch (err) { _handleError(err) }
+
+    var _handleError = function(err) {
+      var returned = self.handleError(err)
+      if (!returned) done(err)
+      else done(null, returned)
+    }
+
+    // Both async and sync validation, in case calling the function directly throws an error.
+    // For asynchronous validation, errors are returned as the first argument of the callback.
+    if (validator.length === 2) {
+      try { validator.call(obj, val, _asyncCb) }
+      catch (err) { _handleError(err) }
+
+    // Synchronous validation only
+    } else {
+      try { validator.call(obj, val) }
+      catch (err) { return _handleError(err) }
+      done()
+    }
   }
-}
+
+})
--- tests.js
+++ tests.js
@@ -2,63 +2,69 @@ var assert = require('assert')
   , _ = require('underscore')
   , chai = require('chai')
   , expect = chai.expect
-  , validationShell = require('./index')
+  , vpod = require('./index')
+
+var ChaiValidator = function() {
+  vpod.Validator.apply(this, arguments)
+}
+
+_.extend(ChaiValidator.prototype, vpod.Validator.prototype, {
+  handleError: function(err) {
+    if (err instanceof chai.AssertionError) return err.message
+  }
+})
+
 
 describe('validation-pod', function() {
 
   describe('validate', function() {
 
+    var validator = new ChaiValidator({
+      validSyncAttr: function(val) {},
+      validAsyncAttr: function(val, done) { done() },
+      unvalidSyncAttr: function(val) { throw new chai.AssertionError('dummy 1') },
+      unvalidAsyncAttr: function(val, done) { done(new chai.AssertionError('dummy 2')) },
+      unvalidAsyncAttrThrowing: function(val, done) { throw new chai.AssertionError('dummy 3') }
+    })
+
+    var dummyObj = {}
+
     it('shouldnt do anything if sync validation succeeds', function(done) {
-      validationErrors = {}
-      validationShell.validate('root.a.b', validationErrors, 1234, function(val) {
-      }, function(err) {
+      validator.validate(dummyObj, 'validSyncAttr', function(err, result) {
         if (err) throw err
-        assert.deepEqual(_.keys(validationErrors), [])
+        assert.ok(!result)
         done()
       })
     })
 
     it('shouldnt do anything if async validation succeeds', function(done) {
-      validationErrors = {}
-      validationShell.validate('root.a.b', validationErrors, 1234, function(val, done) {
-        done()
-      }, function(err) {
+      validator.validate(dummyObj, 'validAsyncAttr', function(err, result) {
         if (err) throw err
-        assert.deepEqual(_.keys(validationErrors), [])
+        assert.ok(!result)
         done()
       })
     })
 
     it('should catch synchronous chai.AssertionErrors', function(done) {
-      validationErrors = {}
-      validationShell.validate('root.a.b', validationErrors, 1234, function(val) {
-        assert.equal(val, 1234)
-        expect(val).to.be.equal(null) // throws an AssertionError
-      }, function(err) {
+      validator.validate(dummyObj, 'unvalidSyncAttr', function(err, result) {
         if (err) throw err
-        assert.deepEqual(_.keys(validationErrors), ['root.a.b'])
+        assert.equal(result, 'dummy 1')
         done()
       })
     })
 
     it('should catch asynchronous chai.AssertionErrors', function(done) {
-      validationErrors = {}
-      validationShell.validate('root.a.b', validationErrors, 1234, function(val, done) {
-        assert.equal(val, 1234)
-        done(new chai.AssertionError('dummy')) // throws an AssertionError
-      }, function() {
-        assert.deepEqual(validationErrors, {'root.a.b': 'dummy'})
+      validator.validate(dummyObj, 'unvalidAsyncAttr', function(err, result) {
+        if (err) throw err
+        assert.equal(result, 'dummy 2')
         done()
       })
     })
 
     it('should catch synchronous chai.AssertionErrors with `func` declared as async', function(done) {
-      validationErrors = {}
-      validationShell.validate('root.a.b', validationErrors, 1234, function(val, done) {
-        assert.equal(val, 1234)
-        expect(val).to.be.equal(null) // throws an AssertionError
-      }, function() {
-        assert.deepEqual(_.keys(validationErrors), ['root.a.b'])
+      validator.validate(dummyObj, 'unvalidAsyncAttrThrowing', function(err, result) {
+        if (err) throw err
+        assert.equal(result, 'dummy 3')
         done()
       })
     })
@@ -70,26 +76,37 @@ describe('validation-pod', function() {
     it('should collect sync and async validation errors', function(done) {
       var validationErrors = {}
         , obj = {attr1: 'bla', attr2: ''}
+        , opts = {validationErrors: validationErrors, prefix: 'root'}
 
-      var checkValidationErrors = function() {
-        assert.deepEqual(_.keys(validationErrors), ['root.attr1', 'root.attr2'])
-        done()
-      }
-
-      validationShell.validateObject('root', obj, validationErrors, { done: checkValidationErrors }, {
+      var validator = new ChaiValidator({
         attr1: function(val) { expect(val).to.be.equal(1) },
         attr2: function(val, done) { done(new chai.AssertionError('dummy')) }
       })
+
+      validator.run(obj, opts, function(err, obj2, validationErrors2) {
+        if (err) throw err
+        assert.deepEqual(_.keys(validationErrors), ['root.attr1', 'root.attr2'])
+        assert.ok(obj === obj2)
+        assert.ok(validationErrors === validationErrors2)
+        done()
+      })
     })
 
-    it('should return a validation error if unknown fields', function() {
+    it('should return a validation error if unknown fields', function(done) {
       var validationErrors = {}
         , obj = {attr1: 'bla', unknown1: 1234, unknown2: 5678}
-      validationShell.validateObject('root', obj, validationErrors, {}, {
+        , opts = {prefix: 'root'}
+
+      var validator = new ChaiValidator({
         attr1: function(val) {},
         attr2: function(val) {}
       })
-      assert.deepEqual(_.keys(validationErrors), ['root'])
+
+      validator.run(obj, opts, function(err, obj2, validationErrors) {
+        assert.ok(obj === obj2)
+        assert.deepEqual(_.keys(validationErrors), ['root'])
+        done()
+      })
     })
 
   })
